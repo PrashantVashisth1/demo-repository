3f4db78f44b8ab71a47df8b72fc1f485c1cec28b
Anshukumar123975 <anshupat2020@gmail.com>
2025-08-21 21:50:47 +0530

    Added functions
    Signed-off-by: Anshukumar123975 <anshupat2020@gmail.com>

---
+++ index.js
@@ -0,0 +1,30 @@
+const addFunction = function(a, b){
+    let result=a+b;
+    return result;
+}
+const subractFunction = function(a, b){
+    let result=a-b;
+    return result;
+}
+const multiplyFunction = function(a, b){
+    let result=a*b;
+    return result;
+}
+const divideFunction = function(a, b){
+    let result=a/b;
+    return result;
+}
+const helloWorldFunction = function(){
+    console.log("Hello World")
+}
+const atobFunction = function(a, b){
+    for(let i=a;i<=b;i++){
+        console.log(i);
+    }
+}
+console.log(addFunction(1,2));
+console.log(subractFunction(3,2));
+console.log(multiplyFunction(2,3));
+console.log(divideFunction(6,2));
+helloWorldFunction();
+atobFunction(1,5);
